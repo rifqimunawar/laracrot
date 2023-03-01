4a561802205cb89afd7a47a00a7e3c5c9de01988
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-01 14:40:53 +0700

    update button in perpus

--- resources/views/user/perpus.blade.php
+++ resources/views/user/perpus.blade.php
@@ -5,7 +5,7 @@
             <div class="row" style="margin-left:0">
                 <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
                     <img src="{{ asset('storage/uploads/download.jfif') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
+                    <div>
                         <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
                     </div>
                 </div>
