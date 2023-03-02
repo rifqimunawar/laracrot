704f9dfb18651d8b734312d5c12a17ceabe69a1f
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-02 23:34:36 +0700

    preview pdf gagal

--- resources/views/user/baca.blade.php
+++ resources/views/user/baca.blade.php
@@ -1,6 +1,6 @@
 {{-- @extends('user.layout')
 @section('content') --}}
   <embed type="application/pdf" src="{{ asset('storage/pdf/download.pdf') }}" width="600" height="400"></embed>
-  {{-- <iframe src="/storage/pdf/download.pdf" width="100%" height="600"></iframe> --}}
+  <iframe src="/storage/pdf/download.pdf" width="600" height="600"></iframe>
 
 {{-- @endsection --}}
