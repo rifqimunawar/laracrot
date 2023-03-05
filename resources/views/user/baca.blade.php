2fa36e55420fe26da4964222cc9c92ac3cd1fffe
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-05 22:36:01 +0700

    ubah tombol baca jadi download

--- resources/views/user/baca.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- @extends('user.layout')
-@section('content') --}}
-  <embed type="application/pdf" src="{{ asset('storage/pdf/download.pdf') }}" width="600" height="400"></embed>
-  <iframe src="/storage/pdf/download.pdf" width="600" height="600"></iframe>
-
-{{-- @endsection --}}
--- resources/views/user/perpus.blade.php
+++ resources/views/user/perpus.blade.php
@@ -7,73 +7,11 @@
                   <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
                     <img src="{{ asset('storage/uploads/'.$perp['image']) }}" style="width: 100%; border-radius:25px">
                     <div class="text-center mt-3">
-                      <a href="/perpus/baca" target="_blank" type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
-                      {{-- <a href="{{ asset('storage/pdf/'.$perp['pdf'])  }}" target="_blank" type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a> --}}
-                      <a href="{{ asset('storage/pdf/'.$perp['pdf'])  }}" download type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Download</a>
+                      <a href="{{ asset('storage/pdf/'.$perp['pdf'])  }}" download type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Download & Baca</a>
                     </div>
                 </div>
               @endforeach 
-                
-{{-- 
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px">
-                    <img src="{{ asset('storage/uploads/25558011._SX318_.jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
-                    <img src="{{ asset('storage/uploads/download.jfif') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse"> 
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
-                    </div>
-                </div>
 
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px">
-                    <img src="{{ asset('storage/uploads/25558011._SX318_.jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
-                    <img src="{{ asset('storage/uploads/download.jfif') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px">
-                    <img src="{{ asset('storage/uploads/25558011._SX318_.jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
-                    <img src="{{ asset('storage/uploads/download.jfif') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px">
-                    <img src="{{ asset('storage/uploads/25558011._SX318_.jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; margin-right:0">
-                    <img src="{{ asset('storage/uploads/1 (5).jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;  border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-
-                <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px">
-                    <img src="{{ asset('storage/uploads/25558011._SX318_.jpg') }}" style="width: 100%; border-radius:25px">
-                    <div class="pt-2 d-flex flex-row-reverse">
-                        <a href="#"type="button" class="btn btn-outline-secondary btn-sm"style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border-radius:10px;">Baca</a>
-                    </div>
-                </div>
-                 --}}
             </div>
         </div>
 @endsection
