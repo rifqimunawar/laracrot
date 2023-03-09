b30462dcd12e3d5bb34fefe7a0fa01974e21a377
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-09 23:15:45 +0700

    add summernote view

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -218,33 +218,7 @@
                   <form>
 
                     <div class="row mb-3">
-                      <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Email Notifications</label>
-                      <div class="col-md-8 col-lg-9">
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="changesMade" checked>
-                          <label class="form-check-label" for="changesMade">
-                            Changes made to your account
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="newProducts" checked>
-                          <label class="form-check-label" for="newProducts">
-                            Information on new products and services
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="proOffers">
-                          <label class="form-check-label" for="proOffers">
-                            Marketing and promo offers
-                          </label>
-                        </div>
-                        <div class="form-check">
-                          <input class="form-check-input" type="checkbox" id="securityNotify" checked disabled>
-                          <label class="form-check-label" for="securityNotify">
-                            Security alerts
-                          </label>
-                        </div>
-                      </div>
+                      <textarea id="summernote"></textarea>
                     </div>
 
                     <div class="text-center">
@@ -297,5 +271,26 @@
 
   </main><!-- End #main -->
 </div>
+<script>
+$('textarea#summernote').summernote({
+  placeholder: 'Sahabat bisa membuat tulisan disini',
+  tabsize: 2,
+  height: 100,
+toolbar: [
+  ['style', ['style']],
+  ['font', ['bold', 'italic', 'underline', 'clear']],
+  // ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
+  //['fontname', ['fontname']],
+ // ['fontsize', ['fontsize']],
+  ['color', ['color']],
+  ['para', ['ul', 'ol', 'paragraph']],
+  ['height', ['height']],
+  ['table', ['table']],
+  ['insert', ['link', 'picture', 'hr']],
+  //['view', ['fullscreen', 'codeview']],
+  ['help', ['help']]
+],
+});
+</script>
 
   @endsection
