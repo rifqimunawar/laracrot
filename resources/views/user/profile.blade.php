90d578b2544c97b7b035a490e88b015af448950f
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-15 04:33:25 +0700

    Update profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -65,8 +65,6 @@
               <div class="tab-content pt-2">
 
                 <div class="tab-pane fade show active profile-overview" id="profile-overview">
-                  <h5 class="card-title">About</h5>
-                  <p class="small fst-italic">Sunt est soluta temporibus accusantium neque nam maiores cumque temporibus. Tempora libero non est unde veniam est qui dolor. Ut sunt iure rerum quae quisquam autem eveniet perspiciatis odit. Fuga sequi sed ea saepe at unde.</p>
 
                   <h5 class="card-title">Profile Details</h5>
 
