81dce79fba1b146ba6b0d4be66489402b58d1456
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-15 04:35:16 +0700

    Update profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -85,7 +85,7 @@
 
                   <div class="row">
                     <div class="col-lg-3 col-md-4 label">Country</div>
-                    <div class="col-lg-9 col-md-8">USA</div>
+                    <div class="col-lg-9 col-md-8">Indonesia</div>
                   </div>
 
                   <div class="row">
