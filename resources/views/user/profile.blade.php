3db8c8fa5963a0c4616ea8577cde7d7dde44b56f
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-15 04:34:55 +0700

    Update profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -74,8 +74,8 @@
                   </div>
 
                   <div class="row">
-                    <div class="col-lg-3 col-md-4 label">Company</div>
-                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                    <div class="col-lg-3 col-md-4 label">Email</div>
+                    <div class="col-lg-9 col-md-8">{{ $user ->email }}</div>
                   </div>
 
                   <div class="row">
