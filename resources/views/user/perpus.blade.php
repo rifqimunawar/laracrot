48c5f49ccf88f28ba2a118b196eb986178ee5323
rifqimunawar <rifqimunawar48@gmail.com>
2023-05-22 17:03:25 +0700

    add search in view perpus

--- resources/views/user/perpus.blade.php
+++ resources/views/user/perpus.blade.php
@@ -4,7 +4,17 @@
 <div class="text-center my-5 pt-3">
   <h4 class="pt-5">Perpustakaan Kader</h4>
         <div class="container card info-card sales-card mt-5">
-            <div class="row" style="margin-left:0">
+            
+          <div class="my-4 col-12 col-sm-8 col-md-6">
+            <form action="" method="get">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="search" placeholder="Search.....">
+                    <button class="btn btn-primary">Search</button>
+                </div>
+            </form>
+        </div>
+
+          <div class="row" style="margin-left:0">
               @foreach ($perpus as $perp)
                   <div class="col-md-2 my-3 mx-2 shadow p-3 mb-5 bg-body-tertiary rounded" style="width:180px; height:290px; object-fit:cover; margin-right:0; position: relative;">
                         <div>
@@ -21,7 +31,25 @@
         </div>
     </div>
 @endsection
-{{-- link buku buku
+
+{{-- <script>
+  // mengambil id id 
+  var search = document.getElementById('search');
+  var tombol = document.getElementById('tombol');
+  var container = document.getElementById('container');
+
+  tombol.addEventListener('click', function () {
+    alert('berhasil');
+  })
+</script> --}}
+
+{{-- 
+  catatan
+  event even di javascript
+  
+  
+  link buku buku
+
   
   https://drive.google.com/drive/folders/1szNFTWR41Y0mKuP1BdBIYS8BqRTzyNhg
 
