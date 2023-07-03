f70045e37c76bd4bbe41b2e34182df92cc53268e
RaiA133 <rai010303@gmail.com@gmail.com>
2023-07-04 00:06:39 +0700

    modal gagal

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -2,38 +2,41 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title> @yield('title') | PMII UNINUS</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title> @yield('title') | PMII UNINUS</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <!-- Favicons -->
-  <link href="{{ asset('assets_user/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('assets_user/img/favicon.png') }}" rel="apple-touch-icon">
+    <!-- Favicons -->
+    <link href="{{ asset('assets_user/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets_user/img/favicon.png') }}" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="/assets/css/style.css" rel="stylesheet">
-  
-  <!-- Template for address -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Template Main CSS File -->
+    <link href="/assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
+    <!-- Template for address -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- =======================================================
   * Template Name: NiceAdmin - v2.5.0
   * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
   * Author: BootstrapMade.com
@@ -52,8 +55,8 @@
 <body>
 
 
-  
-  {{-- <section>
+
+    {{-- <section>
     <div id="myModal" class="modal fade">
       <div class="modal-dialog modal-dialog-centered modal-lg">
           <div class="modal-content">
@@ -64,83 +67,82 @@
               <div class="modal-body">
                 <p>Daftarkan Anggota Kader Dari Masing-Masing Rayon.</p>
                 <img style="width: 100%" src="{{ asset('storage/img/sensus.png') }}" alt="">
-                <a href="#"  target="_blank" class="btn btn-success mt-3 mb-2" 
-                style="width: 100%">Daftarkan Kader!!!</a>
+    <a href="#" target="_blank" class="btn btn-success mt-3 mb-2" style="width: 100%">Daftarkan Kader!!!</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section> --}}
+
+
+
+
+    @include('admin.partials.topbar')
+
+    @include('admin.partials.sidebar')
+
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section dashboard">
+            <div class="row justify-content-center">
+
+                <!-- Left side columns -->
+                <div class="col-lg-12">
+
+                    <div>
+                        @yield('content')
+                    </div>
+                </div><!-- End Left side columns -->
+
+                {{-- @include('admin.partials.rightside') --}}
             </div>
-          </div>
-      </div>
-  </div>
-  </section> --}}
+        </section>
+
+    </main><!-- End #main -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    @include('admin.partials.footer')
+
+    @include('admin.partials.script')
+
+    <!-- Template Main JS File -->
+    <script src="/assets/js/main.js"></script>
+
+    {{-- script sweet alert  --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @include('sweetalert::alert')
 
 
+    {{-- script bootstrap  --}}
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+    </script>
 
+    {{-- script for address  --}}
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 
-@include('admin.partials.topbar')
-
-@include('admin.partials.sidebar')
-
-<main id="main" class="main">
-
-  <div class="pagetitle">
-    <h1>Dashboard</h1>
-    <nav>
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active">Dashboard</li>
-      </ol>
-    </nav>
-  </div><!-- End Page Title -->
-
-  <section class="section dashboard">
-    <div class="row justify-content-center">
-      
-      <a href="{{ route('create.user') }}"  class="btn btn-danger col-lg-8 p-3 m-2"  >
-        <h3> 
-          <strong><b>Sensus Sedang Dilakukan, Daftarkan Kader</b></strong> 
-        </h3>
-      </button> 
-      </a>
-        <!-- Left side columns -->
-        <div class="col-lg-12">
-
-              <div>
-                @yield('content')
-            </div>
-        </div><!-- End Left side columns -->
-        
-        {{-- @include('admin.partials.rightside') --}}
-      </div>
-    </section>
-    
-  </main><!-- End #main -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  @include('admin.partials.footer')
-
-  @include('admin.partials.script')
-  
-  <!-- Template Main JS File -->
-  <script src="/assets/js/main.js"></script>
-
-  {{-- script sweet alert  --}}
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  @include('sweetalert::alert')
-
-
-  {{-- script bootstrap  --}}
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
-  
-  {{-- script for address  --}}
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-  integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-  </script>
 
 </body>
 
