a9964b29ff2be103b36975f9a0a44b50156e66d1
rifqimunawar <rifqimunawar48@gmail.com>
2023-07-10 22:25:03 +0700

    home mobile

--- app/Http/Controllers/MobileHomeController.php
+++ app/Http/Controllers/MobileHomeController.php
@@ -5,16 +5,27 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\User;
 use Inertia\Inertia;
+use App\Models\Galeri;
 use Illuminate\Http\Request;
 
 class MobileHomeController extends Controller
 {
-  public function homepage() {
-   return Inertia::render('Homepage', [
-    'totalMapaba' => User::whereIn('kaderisasi', ['Mapaba', 'PKD', 'PKL', 'PKN'])->count(), // Total mapaba
-    'totalPKD' => User::whereIn('kaderisasi', ['PKD', 'PKL', 'PKN'])->count(),  //total pkd
-    'totalPKL' => User::whereIn('kaderisasi', ['PKL', 'PKN'])->count(),  //total pkd
-    'totalPKN' => User::whereIn('kaderisasi', ['PKN'])->count(),  //total pkd
-   ]);
-  }
+ public function homepage()
+ {
+     $users = User::with('rayon')->latest()->paginate(5);
+     $galeries = Galeri::with('user')->where('status', 1)->latest()->get();
+ 
+     // dd($galeries);
+     return Inertia::render('Homepage', [
+         'totalMapaba' => User::whereIn('kaderisasi', ['Mapaba', 'PKD', 'PKL', 'PKN'])->count(),
+         'totalPKD' => User::whereIn('kaderisasi', ['PKD', 'PKL', 'PKN'])->count(),
+         'totalPKL' => User::whereIn('kaderisasi', ['PKL', 'PKN'])->count(),
+         'totalPKN' => User::whereIn('kaderisasi', ['PKN'])->count(),
+         'users' => $users,
+         'galeries' => $galeries,
+     ]);
+ }
+ 
+ 
+ 
 }
