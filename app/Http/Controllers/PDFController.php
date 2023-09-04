e74950e16daba75f8ee72bd22ce9193235063e69
rifqimunawar <rifqimunawar48@gmail.com>
2023-09-04 13:38:56 +0700

    test kta

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -12,11 +12,6 @@ use Illuminate\Support\Facades\View;
 class PDFController extends Controller
 {
 
- public function viewPDF($user) {
-  $user = User::find($user);
-   return view('admin.user.pdf',['user'=>$user]);
- }
-
  public function rayonPDF($slug, Request $request)
  {
      // Ambil data rayon berdasarkan slug
@@ -38,38 +33,11 @@ class PDFController extends Controller
  
      return view('admin.rayon.rayon-pdf', compact('rayon', 'count_user', 'now'));
  }
- 
 
- // public function downloadPDF($user)
- //    {
-     
- //        // Membuat objek PDF
- //        $pdf = new Dompdf(); // atau new TCPDF()
-        
- //        // Mendapatkan data yang akan ditampilkan dalam PDF
- //        $data = [
- //            'title' => 'Kader PMII UNINUS',
- //            'content' => $user->name,
- //        ];
-        
- //        // Membuat tampilan (view) dengan data yang diberikan
- //        $view = View::make('admin.user.pdf', $data);
-        
- //        // Render tampilan ke dalam HTML
- //        $html = $view->render();
-        
- //        // Memuat HTML ke dalam objek PDF
- //        $pdf->loadHtml($html);
-        
- //        // Menetapkan opsi PDF jika diperlukan (misalnya ukuran kertas, orientasi, dll.)
- //        $pdf->setPaper('A4', 'portrait');
-        
- //        // Render PDF
- //        $pdf->render();
-        
- //        // Menghasilkan file PDF dengan nama yang unik
- //        // $filename = 'PMII UNINUS' . $user->name . '.pdf';
- //        // $pdf->stream($filename, ['Attachment' => false]);
-    
- //    }
+
+ public function kaderPDF($id, Request $request) {
+  $users = User::findOrFail($id);
+
+  return view('admin.user.pdf', compact('users'));
+ }
 }
