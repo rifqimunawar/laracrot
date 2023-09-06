ab39287868162c5a132919c837bd02e8060cffbd
rifqimunawar <rifqimunawar48@gmail.com>
2023-09-06 17:23:24 +0700

    perbaikan link kta

--- resources/views/admin/user/pdf.blade.php
+++ resources/views/admin/user/pdf.blade.php
@@ -59,7 +59,7 @@
       </div>
 
       <div class="qr">
-       {!! QrCode::size(100)->generate("/qrcode/varifikasi/kta/{$users->id}/anjay/mabar/ckuahsksdfsihew/S3NAT-4NJ1NG-63lut-73ng/51-3nd1") !!}
+       {!! QrCode::size(100)->generate("https://pmiiuninus.com/qrcode/varifikasi/kta/{$users->id}/anjay/mabar/ckuahsksdfsihew/S3NAT-4NJ1NG-63lut-73ng/51-3nd1") !!}
       </div>
 
     </div>
